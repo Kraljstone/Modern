7c69d6e0b7f33b1898744edfa9e2e760e2c83443
AENIMA <aenima@AENIMAs-MacBook-Pro.local>
2023-05-03 18:32:09 +0200

    ngcc error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './Components/login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginFormComponent } from './Components/login-form/login-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, LoginFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
     FontAwesomeModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
