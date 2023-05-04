352ac989463a41301d9124e08fca52e0432679d3
AENIMA <aenima@AENIMAs-MacBook-Pro.local>
2023-05-04 18:00:52 +0200

    form finished

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -1,21 +1,50 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { LoggingService } from 'src/app/Services/logging.service';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(control && control.invalid && (control.dirty || isSubmitted));
   }
 }
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
+  providers: [{ provide: ErrorStateMatcher, useClass: MyErrorStateMatcher }],
 })
 export class LoginFormComponent {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  emailFormControl = new FormControl('', [
+    Validators?.required,
+    Validators?.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators?.required,
+    Validators?.pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ),
+  ]);
 
   matcher = new MyErrorStateMatcher();
+
+  constructor(private loggingService: LoggingService) {}
+
+  login(event: Event) {
+    event.preventDefault();
+    this.loggingService.logStatusChange(
+      this.emailFormControl.status,
+      this.passwordFormControl.status
+    );
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { LoggingService } from './Services/logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LoggingService],
 })
 export class AppComponent {
-  title = 'Modern';
+  isLogged = true;
+
+  constructor(private loggingService: LoggingService) {}
+
+  ngOnInit() {
+    this.loggingService.isLogged.subscribe((isLogged) => {
+      this.isLogged = isLogged;
+    });
+  }
 }
